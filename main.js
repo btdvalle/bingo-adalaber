24ad7649638652fe0852e38dbe8042dfbe9d7cdd
btdvalle <btdvalle@gmail.com>
2019-08-06 10:28:45 +0200

    Add F8

--- main.js
+++ main.js
@@ -5,19 +5,45 @@ const btn = document.querySelector(".js-btn");
 const newContainer = document.querySelector(".js-newball");
 const modal = document.querySelector(".js-modal");
 
-const getCardNumbers = function() {
-  for (let i = 0; i < 20; i++) {
-    cardNumbers[i] = i;
-    card.innerHTML += `<div class="number number${i}"> ${cardNumbers[i]}</div>`;
+const getAleatory = function(arr, long) {
+  arr[0] = Math.round(Math.random() * 99 + 1);
+  for (let i = 1; i < long; i++) {
+    arr[i] = Math.round(Math.random() * 99 + 1);
+    for (let j = 0; j < i; j++) {
+      if (arr[i] === arr[j]) {
+        arr[i] = Math.round(Math.random() * 99 + 1);
+        i--;
+      }
+    }
   }
 };
 
-let newNumber = 0;
-const getNewNumber = function() {
-  newContainer.innerHTML += `<div class="number">  ${newNumber}</div>`;
+// const getAleatory2 = function(arr, long) {
+//   while (arr.length < long) {
+//     const randomNumber = Math.round(Math.random() * 99 + 1);
+//     if (arr.indexOf(arr, randomNumber) === -1) {
+//       arr.push(randomNumber);
+//     }
+//   }
+// };
+
+const getCardNumbers = function() {
+  getAleatory(cardNumbers, 3);
+  for (let i = 0; i < cardNumbers.length; i++) {
+    card.innerHTML += `<div class="number number${i}">  ${cardNumbers[i]}</div>`;
+  }
 };
-const masUno = function() {
-  newNumber++;
+
+const newNumbers = [];
+let newNumber;
+getAleatory(newNumbers, 100);
+let i = 0;
+const getNewNumber = function() {
+  if (i < 100) {
+    newContainer.innerHTML += `<div class="number">  ${newNumbers[i]}</div>`;
+    newNumber = newNumbers[i];
+  }
+  i++;
 };
 
 const findMatched = function() {
@@ -34,17 +60,13 @@ const showUpModal = function() {
 };
 
 const isBingo = function() {
-  //   debugger;
-  let bingo;
-  for (let i = 0; i < cardNumbers.length; i++) {
-    const number = document.querySelector(`.number${i}`);
-    if (number.classList.contains("matched")) {
-      bingo = true;
-    } else {
-      return (bingo = false);
+  let allMatches = true;
+  for (let idx = 0; idx < cardNumbers.length; idx++) {
+    if (newNumbers.slice(0, i).indexOf(cardNumbers[idx]) === -1) {
+      allMatches = false;
     }
   }
-  if (bingo === true) {
+  if (allMatches === true) {
     btn.classList.add("hidden");
     showUpModal();
   }
@@ -53,7 +75,6 @@ const isBingo = function() {
 const bingo = function() {
   getNewNumber();
   findMatched();
-  masUno();
   isBingo();
 };
 
