f80e9cc0b957bea9e86743473ab592d12c45e7ec
info@migueldelmazo.com <migueldelmazo@gmail.com>
2019-08-09 13:14:23 +0200

    Add improvements commets

--- main.js
+++ main.js
@@ -1,93 +1,101 @@
-"use strict";
-const card = document.querySelector(".js-card");
-const btn = document.querySelector(".js-btn");
-const newContainer = document.querySelector(".js-newball");
-const modal = document.querySelector(".js-modal");
+'use strict';
+const card = document.querySelector('.js-card');
+const btn = document.querySelector('.js-btn');
+const newContainer = document.querySelector('.js-newball');
+const modal = document.querySelector('.js-modal');
 const cardNumbers = [];
 const newNumbers = [];
 
+// esto lo podemos sacar a una función independiente para que sea más legible
+const getRandomNumber = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+// creo que esto funcionaría así igual que la tuya
 const getAleatory = function(arr, long) {
-  arr[0] = Math.round(Math.random() * 99 + 1);
   for (let i = 1; i < long; i++) {
-    arr[i] = Math.round(Math.random() * 99 + 1);
-    for (let j = 0; j < i; j++) {
-      if (arr[i] === arr[j]) {
-        arr[i] = Math.round(Math.random() * 99 + 1);
-        i--;
-      }
+    const randomNumber = getRandomNumber(100);
+    if (arr.indexOf(randomNumber) >= 0) {
+      i--;
+    } else {
+      arr.push(randomNumber);
     }
   }
 };
 
-// const getAleatory2 = function(arr, long) {
-//   while (arr.length < long) {
-//     const randomNumber = Math.round(Math.random() * 99 + 1);
-//     if (arr.indexOf(arr, randomNumber) === -1) {
-//       arr.push(randomNumber);
-//     }
-//   }
-// };
-
+// esta ok
 const getCardNumbers = function() {
   getAleatory(cardNumbers, 20);
   for (let i = 0; i < cardNumbers.length; i++) {
-    card.innerHTML += `<div class="number number${i}">  ${cardNumbers[i]}</div>`;
+    card.innerHTML += `<div class="number number${i}">${cardNumbers[i]}</div>`;
   }
 };
 
 let newNumber;
 let i = 0;
+// aquí hay cosas que no me terminan de encajar:
+// - has generado 100 números aleatorios en newNumbers y con la variable i estás diciendo cuantos has mostrado
+// - lo primero sería cambiar la variable i por newNumbersShown porque "i" no significa lo que es esa variable
+// - pero estás gestionando el concepto "Números que han salido del bingo" con dos variables: "newNumbers" y "i"
+// - es más fácil no usar "i"
+// - es más fácil arrancar la web con 0 números en newNumbers y cada vez que ejecutamos la función "bingo" añadir un nuevo número aleatorio a newNumbers
+// - para generar un solo número aleatorio más utilizando las funciones que tenías puedes hacer getAleatory(newNumbers, newNumbers.length + 1)
 const getNewNumber = function() {
   if (i < 100) {
-    newContainer.innerHTML += `<div class="number">  ${newNumbers[i]}</div>`;
+    newContainer.innerHTML += `<div class="number">${newNumbers[i]}</div>`;
     newNumber = newNumbers[i];
   }
   i++;
+  console.log(newNumbers.length, i);
 };
 
+// esta ok
 const findMatched = function() {
   for (let i = 0; i < cardNumbers.length; i++) {
     if (newNumber === cardNumbers[i]) {
       const number = document.querySelector(`.number${i}`);
-      number.classList.add("matched");
+      number.classList.add('matched');
     }
   }
 };
 
+// esta ok
 const showUpModal = function() {
-  modal.classList.add("showup");
+  modal.classList.add('showup');
 };
 
+// esta ok
 const isBingo = function() {
   let allMatches = true;
   for (let idx = 0; idx < cardNumbers.length; idx++) {
     if (newNumbers.slice(0, i).indexOf(cardNumbers[idx]) === -1) {
       allMatches = false;
-      const play = function() {
-        setInterval(bingo, 1000);
-      };
-      btn.addEventListener("click", play);
     }
   }
-  if (allMatches === true) {
-    btn.classList.add("hidden");
+  return allMatches;
+};
+
+// esta ok
+const bingo = function() {
+  getNewNumber();
+  findMatched();
+  if (isBingo()) {
+    btn.classList.add('hidden');
     showUpModal();
     clearInterval(stop);
   }
 };
 
-const bingo = function() {
-  getNewNumber();
-  findMatched();
-  isBingo();
-};
-// btn.addEventListener("click", bingo);
-
+// esta ok
 let stop;
 const play = function() {
-  stop = setInterval(bingo, 1000);
+  stop = setInterval(bingo, 100);
 };
 
+// esta ok
 getCardNumbers();
 getAleatory(newNumbers, 100);
-btn.addEventListener("click", play);
+btn.addEventListener('click', play);
+
+// las mejoras que te propongo son para que avances un escalón más en tu nivel como programadora
+// pero el ejercicio está muy bien
